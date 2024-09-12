0e0edc07752817452063311db1e928ce9cf317fb
gmarinan <gabrielmarinan@gmail.com>
2024-09-11 22:09:53 -0300

    Primera configuración de pipelines y variables

---
+++ security-pipeline.groovy
@@ -0,0 +1,31 @@
+pipeline {
+    agent any
+    environment {
+        var1 = ''
+        var2 = ''
+    }
+    stages {
+        stage('Setup Variables') {
+            steps {
+                script {
+                    // Leer el archivo YAML
+                    def config = readYaml file: 'security-pipeline-variables.yml'
+                    def pipelineConfig = config[env.JOB_NAME]
+                    
+                    if (pipelineConfig) {
+                        env.var1 = pipelineConfig.var1
+                        env.var2 = pipelineConfig.var2
+                    } else {
+                        error "No configuration found for job ${env.JOB_NAME}"
+                    }
+                }
+            }
+        }
+        stage('Build') {
+            steps {
+                echo "Usando var1: ${env.var1} y var2: ${env.var2}"
+                // Lógica de construcción aquí
+            }
+        }
+    }
+}
